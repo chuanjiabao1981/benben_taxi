c434eef76dea300d5be65b201823a5aa64c00aa5
guowei <chuanjiabao1981@gmail.com>
2013-06-01 16:30:12 +0800

    passenger show taxi requests

--- script/post.rb
+++ script/post.rb
@@ -4,7 +4,7 @@ require "base64"
 class TestApi
 	def initialize
 		@host = 'localhost'
-		@host = 'v2.365check.net'
+		#@host = 'v2.365check.net'
 		@port = '8081'
 	end
 
@@ -30,11 +30,12 @@ class TestApi
 	end
 
 	def show_taxi_request_api
-		request_header 	= self.get_driver_head
+		#request_header 	= self.get_driver_head
 		taxi_request   = JSON.parse(self.create_taxi_request_api)
 		path 			= "/api/v1/taxi_requests/#{taxi_request["id"]}"
 		#path 			= "/api/v1/taxi_requests/31	"
-		self.get_request(path,request_header)
+		self.get_request(path,self.get_driver_head)
+		self.get_request(path,self.get_passenger_head)
 	end
 	def answer_taxi_request_api(taxi_request=nil)
 		request_header = self.get_driver_head
@@ -80,7 +81,7 @@ class TestApi
 	end
 	def taxi_requests_index_api
 		path ="/api/v1/taxi_requests?lat=8&lng=8&radius=10"
-		request_header = self.get_passenger_head
+		request_header = self.get_driver_head
 		self.get_request(path,request_header)
 	end
 	def driver_signin_api
@@ -127,8 +128,8 @@ s = TestApi.new
 #s.create_driver_track_point_api
 #s.taxi_requests_index_api
 #s.answer_taxi_request_api
-#s.show_taxi_request_api
+s.show_taxi_request_api
 #s.cancel_taxi_request_api
-s.confirm_taxi_request_api
+#s.confirm_taxi_request_api
 
 
