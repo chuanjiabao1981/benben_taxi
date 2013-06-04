41a69af9c2e3e45e0811281ec0f96cb3894c13d9
guowei <chuanjiabao1981@gmail.com>
2013-06-04 19:49:01 +0800

    passenger get nearby driver

--- script/post.rb
+++ script/post.rb
@@ -39,8 +39,9 @@ class TestApi
 
 	def get_latest_drvier_api
 		path 			= "/api/v1/users/nearby_driver?lat=8&lng=8"
-		create_driver_track_point_api("15910676326")
-		create_driver_track_point_api("13810025096")
+		path 			= "/api/v1/users/nearby_driver?lat=22.540545&lng=113.959805"
+		#create_driver_track_point_api("15910676326")
+		#create_driver_track_point_api("13810025096")
 		self.get_request(path,self.get_passenger_head)
 	end
 	def answer_taxi_request_twice
@@ -138,14 +139,14 @@ end
 
 s = TestApi.new
 #s.passenger_signin_api
-#s.create_taxi_request_api
+s.create_taxi_request_api
 #s.create_driver_track_point_api
-s.taxi_requests_index_api
+#s.taxi_requests_index_api
 #s.answer_taxi_request_api
 #s.show_taxi_request_api
 #s.cancel_taxi_request_api
 #s.confirm_taxi_request_api
 #s.answer_taxi_request_twice
-#s.get_latest_drvier_api
+s.get_latest_drvier_api
 
 
--- script/taxi_request_timeout_check.rb
+++ script/taxi_request_timeout_check.rb
@@ -11,8 +11,9 @@ class TaxiRequestTimeoutCheck
 	end
 	def check
 		loop do
-			add_driver_track_point
 			check_time_out_taxi_requests
+			response_taxi_request
+			add_driver_track_point
 			sleep(10)
 		end
 	end
@@ -50,6 +51,34 @@ class TaxiRequestTimeoutCheck
 			end
 		end
 	end
+	def response_taxi_request
+		TaxiRequest.by_state.each do |taxi_request|
+			driver_mobile 		 = @driver_mobiles[0]
+			current_driver	 	 = get_current_driver(driver_mobile)
+			params = {
+						taxi_response:{
+							driver_mobile: driver_mobile,
+							driver_lat: get_random(@driver_lat),
+							driver_lng: get_random(@driver_lng)
+						}
+					}
+			if current_driver
+				taxi_request.driver_response(params[:taxi_response],current_driver)
+				if taxi_request.state == 'Waiting_Passenger_Confirm' 
+					logger.info "#{driver_mobile} response the taxi_request #{taxi_request.id}"
+				end
+			else
+				logger.warn "No Driver Found #{driver_mobile} to response the taxi request #{taxi_request.id}"
+			end
+
+		end
+	end
+	def get_random(val)
+		 val + Random.new.rand(0..50)/100000.0
+	end
+	def get_current_driver(mobile)
+		User.find_by mobile: mobile, role: "driver"
+	end
 end
 
 
@@ -60,7 +89,8 @@ daemon_options = {
   :multiple   => false,
   :dir_mode   => :normal,
   :dir        => File.join(dir, 'tmp', 'pids'),
-  :backtrace  => true
+  :backtrace  => true,
+  :monitor    => true
 }
 RAILS_RUNNING_ENV=File.expand_path(File.join(File.dirname(__FILE__), '..', 'config', 'environment'))
 
@@ -71,8 +101,8 @@ Daemons.run_proc('TaxiRequestTimeoutCheck', daemon_options) do
   else
     ARGV.clear
   end
+  ENV["RAILS_ENV"] 		||=  "production"
   Dir.chdir dir
   require  RAILS_RUNNING_ENV
-  puts ""
   TaxiRequestTimeoutCheck.new.check
 end
